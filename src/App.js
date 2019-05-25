2f89c9095d1fbec2c2cbb0e37d2c9c7584efb068
CesarRivasP <cesarjose58@gmail.com>
2019-05-25 19:40:52 -0400

    Pass Title for header component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Header from './components/header';
 class App extends Component {
   render(){
     return (
-      <div className="app">
-      <Header title="Clima" />
+      <div className="container">
+        <Header title="Clima" />
       </div>
     );
   }
