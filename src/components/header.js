6c8aa683eb8e7be30da16d030968e03edb9fbfda
CesarRivasP <cesarjose58@gmail.com>
2019-05-25 19:41:16 -0400

    Implemented app bar for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const styles = {
+  headerBar: {
+    backgroundColor: '#1e3799',
+    color: 'white',
+    height: 55,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+};
+
 
 const Header = (props) => {
+  const { classes, title } = props;
   return (
-    <div>
-      <header className="">
-
-      </header>
-    </div>
+    <header>
+        <AppBar position="static" color="default" className={classes.headerBar}>
+          <Toolbar>
+            <h1>{ title }</h1>
+          </Toolbar>
+      </AppBar>
+    </header>
   )
 }
 
-export default Header;
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+
+export default withStyles(styles)(Header);
