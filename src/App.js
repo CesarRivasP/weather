24ed558fb18dd5e02d6172878f38e9e2f3acc4d5
CesarRivasP <cesarjose58@gmail.com>
2019-05-26 13:14:44 -0400

    Implemented error component and chages of error state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from './components/header';
 import Form from './components/form';
+import Error from './components/error';
 
 class App extends Component {
-  render(){
+  state = {
+    error: false
+  }
+
+  queryData = (response) => {
+
+    if(response.city === '' || response.country === ''){
+      console.log('Hay errores');
+      this.setState({ error: true })
+    }
+    else {
+      console.log(response);
+      this.setState({ error: false })
+    }
+  }
+
+  render() {
+
+    const error = this.state.error;
+
+    let result;
+
+    if(error){
+      result = <Error message="Ambos campos son obligatorios" />;
+    }
+
     return (
       <div>
-        <Header title="Clima" />
-        <Form />
+        <Header title="Clima"/>
+        <Form queryData={this.queryData} />
+        { result }
       </div>
     );
   }
