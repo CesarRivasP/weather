4e798cf581216987805f051defcd0c28715ade64
CesarRivasP <cesarjose58@gmail.com>
2019-05-25 22:09:02 -0400

    implemented form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/header';
+import Form from './components/form';
 
 class App extends Component {
   render(){
     return (
       <div className="container">
         <Header title="Clima" />
+        <Form />
       </div>
     );
   }
