74f0a2a222a60a55985da34426fa7dd9fbf79901
CesarRivasP <cesarjose58@gmail.com>
2019-05-26 12:23:36 -0400

    Create and implemented refs

--- src/components/form.js
+++ src/components/form.js
@@ -1,56 +1,81 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 const countrys = [
-  { id: 'AR', name: 'Argentina' },
-  { id: 'CO', name: 'Colombia' },
-  { id: 'CR', name: 'Costa Rica' },
-  { id: 'ES', name: 'España' },
-  { id: 'MX', name: 'Mexico' },
-  { id: 'PE', name: 'Peru' },
-  { id: 'US', name: 'Estados Unidos' },
-  { id: 'VE', name: 'Venezuela' }
-]
+  {
+    id: 'AR',
+    name: 'Argentina'
+  }, {
+    id: 'CO',
+    name: 'Colombia'
+  }, {
+    id: 'CR',
+    name: 'Costa Rica'
+  }, {
+    id: 'ES',
+    name: 'España'
+  }, {
+    id: 'MX',
+    name: 'Mexico'
+  }, {
+    id: 'PE',
+    name: 'Peru'
+  }, {
+    id: 'US',
+    name: 'Estados Unidos'
+  }, {
+    id: 'VE',
+    name: 'Venezuela'
+  }
+];
 
 class Form extends Component {
 
+  cityRef = React.createRef()
+  countryRef = React.createRef()
 
-  render(){
-    return(
-      <div className="card mt-5 ml-5 mr-5 p-4">
-        <form >
-          <div className="form-group row d-flex justify-content-center align-items-center mb-0">
-            <label className="col-sm-4 col-lg-1 col-form-label pr-0" htmlFor="city">Ciudad</label>
-            <div className="col-sm-8 col-lg-2">
-              <input type="email" className="form-control" id="city" placeholder="Caracas" />
-            </div>
-            <label className="col-sm-4 col-lg-1 col-form-label pr-0" htmlFor="country">Pais</label>
-            <div className="col-sm-8 col-lg-2">
-              <select className="form-control" id="country">
-                {
-                  countrys.map((country) => (
-                      <option key={country.id} value={country.id}>{country.name}</option>
-                    )
-                  )
-                }
-             </select>
-            </div>
-            <button
-              type="submit"
-              className="btn col-sm-4 col-lg-1 mr-3 ml-5"
-              style={{ color: 'white', backgroundColor: '#218c74'}}>
-              Buscar
-            </button>
+  searchWeather = (e) => {
+    e.preventDefault();
+
+    // leer los refs
+    const response = {
+      city: this.cityRef.current.value,
+      country: this.countryRef.current.value
+    }
+    console.log(response);
+    // enviar props
+
+    // resetear el form
+  }
+
+  render() {
+    return (<div className="card mt-5 ml-5 mr-5 p-4">
+      <form onSubmit={this.searchWeather}>
+        <div className="form-group row d-flex justify-content-center align-items-center mb-0">
+          <label className="col-sm-4 col-lg-1 col-form-label pr-0" htmlFor="city">Ciudad</label>
+          <div className="col-sm-8 col-lg-2">
+            <input type="text" className="form-control" id="city" placeholder="Caracas" ref={this.cityRef} />
           </div>
-        </form>
-      </div>
-    )
+          <label className="col-sm-4 col-lg-1 col-form-label pr-0" htmlFor="country">País</label>
+          <div className="col-sm-8 col-lg-2">
+            <select className="form-control" id="country" ref={this.countryRef}>
+              {countrys.map((country) => (<option key={country.id} value={country.id}>{country.name}</option>))}
+            </select>
+          </div>
+          <button type="submit" className="btn col-sm-4 col-lg-1 mr-3 ml-5" style={{
+              color: 'white',
+              backgroundColor: '#218c74'
+            }}>
+            Buscar
+          </button>
+        </div>
+      </form>
+    </div>)
   }
 }
 
 // Form.propTypes = {
 //
 // };
-
 
 export default Form;
