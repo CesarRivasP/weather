286e8c270fc4d084f7a3d1a357df9f50a65dafd8
CesarRivasP <cesarjose58@gmail.com>
2019-05-25 22:17:34 -0400

    implemented form

--- src/components/form.js
+++ src/components/form.js
@@ -1,107 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
 
-const useStyles = makeStyles(theme => ({
-  card: {
-    minWidth: 275,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-  },
-  textField: {
-   width: 200,
-   display: 'flex',
-   justifyContent: 'center',
-   alignItems: 'center'
-   ,
-   flexDirection: 'column',
- },
- formControl: {
-    minWidth: 275,
-    padding: '0px 0px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-  },
-}));
-
-const classes = useStyles();
 
 class Form extends Component {
 
-    state = {
-     city: '',
-     values: ''
-   };
-
-   handleChange = name => event => {
-     this.setState({ [name]: event.target.value });
-   };
-
-
-  handleSelectChange = (event) =>  {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
 
   render(){
     return(
-      <div className="container">
-         <Card className={classes.card}>
-           <CardContent>
-             <form /*onSubmit={}*/ noValidate autoComplete="off">
-             <div>
-               <TextField
-                 id="standard-name"
-                 label="Ciudad"
-                 className={classes.textField}
-                 value={this.state.city}
-                 onChange={this.handleChange('city')}
-                 margin="normal"
-               />
-             </div>
-             <div>
-               <FormControl className={classes.formControl}>
-                 <InputLabel htmlFor="age-simple">Age</InputLabel>
-                 <Select
-                   value={this.state.values}
-                   onChange={this.handleSelectChange}
-                   inputProps={{
-                     name: 'age',
-                     id: 'age-simple',
-                   }}
-                   >
-                     <MenuItem value="">
-                       <em>None</em>
-                     </MenuItem>
-                     <MenuItem value={10}>Ten</MenuItem>
-                     <MenuItem value={20}>Twenty</MenuItem>
-                     <MenuItem value={30}>Thirty</MenuItem>
-                   </Select>
-                 </FormControl>
-             </div>
-             </form>
-           </CardContent>
-         </Card>
+      <div className="card mt-5">
+        <form >
+          <div className="form-group row">
+            <label className="col-sm-4 col-lg-4 col-form-label">Nombre de Mascota</label>
+            <div className="col-sm-8 col-lg-8">
+              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+          </div>
+          <div className="form-group row">
+            <label className="col-sm-4 col-lg-4 col-form-label">Nombre del Dueño</label>
+            <div className="col-sm-8 col-lg-8">
+              <select class="form-control" id="exampleFormControlSelect1">
+               <option>1</option>
+               <option>2</option>
+               <option>3</option>
+               <option>4</option>
+               <option>5</option>
+             </select>
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
 }
 
-Form.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+// Form.propTypes = {
+//
+// };
 
 
 export default Form;
