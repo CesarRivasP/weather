98a6dac725dfd17e96f113c2786074a0a99d2e95
CesarRivasP <cesarjose58@gmail.com>
2019-05-27 21:01:24 -0400

    Delete without using packages

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles } from '@material-ui/styles';
-import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   headerBar: {
